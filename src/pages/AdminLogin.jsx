dab2f84e002639eadb525c37caf4cb808395d302
John Angel Henao <john.alvarez@correounivalle.edu.co>
2025-03-08 13:49:24 -0500

    css rename adminlogin

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
-import "../css/adminLogin.css";
+import "../css/adminlogin.css";
 
 function AdminLogin() {
   const { login } = useAuth();
